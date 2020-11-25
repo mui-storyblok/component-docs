036a559a7a855d845449a2f929eb7d7ff94d19fa
JoscelynJames <josce.james7@gmail.com>
2020-11-24 21:46:24 -0700

    redircts and save component data on click

--- src/components/ComponentList/ComponentList.js
+++ src/components/ComponentList/ComponentList.js
@@ -3,12 +3,14 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-const handleClick = component => {
-  const origin = window.location.origin
-  window.location.href = `${origin}/page-${component.name}`
-}
-
 function ComponentList() {
+
+  const handleClick = component => {
+    const origin = window.location.origin
+    window.localStorage.setItem('componentData', JSON.stringify(component))
+    window.location.href = `${origin}/page-${component.name.toLowerCase()}`
+  }
+
   return (
     <List aria-label="main mailbox folders">
       {components.map(component => {
