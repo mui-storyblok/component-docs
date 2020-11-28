bb558c00f711d3dcc72905d77fe0e21e8ed43043
JoscelynJames <josce.james7@gmail.com>
2020-11-28 16:42:10 -0700

    updated styling in header

--- src/components/SbComponentDocumentation/SbComponentDocumentation.js
+++ src/components/SbComponentDocumentation/SbComponentDocumentation.js
@@ -1,18 +1,32 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 import Configuration from './Configuration';
 
 const componentData = JSON.parse(window.localStorage.getItem('componentData'))
+const useStyles = makeStyles(() => ({
+  divider: {
+    margin: '16px 0'
+  },
+  grid: {
+    margin: '16px 0'
+  }
+}));
 
 function SbComponentDocumentation() {
+  const classes = useStyles();
+
   return (  
-    <Grid container direction="column">
+    <Grid container direction="column" className={classes.grid}>
       <Typography 
-        variant="h5" 
+        variant="h4" 
         align="left">
           { componentData.component_group_name } / { componentData.name }
       </Typography>
+      
+      <Divider className={classes.divider}/>
 
       <Configuration config={componentData.schema} />
     </Grid>
