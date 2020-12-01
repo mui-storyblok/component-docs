26439881f2f1a1dacbdb50a68cc27f5891e505c5
alex james <apjames93@gmail.com>
2020-12-01 12:47:57 -0700

    added router to route to App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact>
+        <App />
+      </Route>
+      <Route path="/page**" exact>
+        <App />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
